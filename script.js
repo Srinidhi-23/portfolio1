abe0f605a4d80c6ef9188d0313a2264763090870
Srinidhi-23 <22311a05k2@cse.sreenidhi.edu.in>
2025-05-06 11:42:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Message sent! (Form functionality is not connected to a backend)");
+});
